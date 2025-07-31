a86e90e128f6ea11fa14637d0bbdbc259e8d2513
Selçuk Talha Kul <157029095+selcuktk@users.noreply.github.com>
2025-07-31 07:24:32 +0300

    feat: PAckageModel

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -1,7 +1,7 @@
-
 from pydantic import Field, validator
 from typing import List, Optional, Union, Literal
-from sdks.novavision.src.base.model import Package, Image, Inputs, Configs, Outputs, Response, Request, Output, Input, Config
+from sdks.novavision.src.base.model import Package, Image, Inputs, Configs, Outputs, Response, Request, Output, Input, \
+    Config, Detection
 
 
 class InputImage(Input):
@@ -21,9 +21,18 @@ class InputImage(Input):
         title = "Image"
 
 
+class InputDetections(Input):
+    name: Literal["inputDetections"] = "inputDetections"
+    value: List[Detection]
+    type: str = "list"
+
+    class Config:
+        title = "Detections"
+
+
 class OutputImage(Output):
     name: Literal["outputImage"] = "outputImage"
-    value: Union[List[Image],Image]
+    value: Union[List[Image], Image]
     type: str = "object"
 
     @validator("type", pre=True, always=True)
@@ -38,69 +47,112 @@ class OutputImage(Output):
         title = "Image"
 
 
-class KeepSideFalse(Config):
-    name: Literal["False"] = "False"
-    value: Literal[False] = False
-    type: Literal["bool"] = "bool"
-    field: Literal["option"] = "option"
-
-    class Config:
-        title = "Disable"
-
-
-class KeepSideTrue(Config):
-    name: Literal["True"] = "True"
-    value: Literal[True] = True
-    type: Literal["bool"] = "bool"
-    field: Literal["option"] = "option"
-
-    class Config:
-        title = "Enable"
-
-
-class KeepSideBBox(Config):
+class KernelSize(Config):
     """
-        Rotate image without catting off sides.
+    Low blur level: 1-9
+    Mediam blur level: 9-21
+    High Blur level: >21
     """
-    name: Literal["KeepSide"] = "KeepSide"
-    value: Union[KeepSideTrue, KeepSideFalse]
-    type: Literal["object"] = "object"
-    field: Literal["dropdownlist"] = "dropdownlist"
-
-    class Config:
-        title = "Keep Sides"
-
-
-class Degree(Config):
-    """
-        Positive angles specify counterclockwise rotation while negative angles indicate clockwise rotation.
-    """
-    name: Literal["Degree"] = "Degree"
-    value: int = Field(ge=-359.0, le=359.0,default=0)
+    name: Literal["KernelSize"] = "KernelSize"
+    value: int = Field(ge=1, le=21, default=5)
     type: Literal["number"] = "number"
     field: Literal["textInput"] = "textInput"
-    placeHolder: Literal["[-359, 359]"] = "[-359, 359]"
+
+    @validator("value")
+    def check_odd_value(cls, v: int):
+        if v % 2 == 0:
+            raise ValueError("Kernel size must be an odd number.")
+        return v
 
     class Config:
-        title = "Angle"
+        title = "Kernel Size"
 
 
-class PackageInputs(Inputs):
+class BlurGaussian(Config):
+    kernelSize: KernelSize
+    name: Literal["BlurGaussian"] = "BlurGaussian"
+    value: Literal["BlurGaussian"] = "BlurGaussian"
+    type: Literal["string"] = "string"
+    field: Literal["option"] = "option"
+
+    class Config:
+        title = "Gaussian"
+
+
+class BlurAverage(Config):
+    kernelSize: KernelSize
+    name: Literal["BlurAverage"] = "BlurAverage"
+    value: Literal["BlurAverage"] = "BlurAverage"
+    type: Literal["string"] = "string"
+    field: Literal["option"] = "option"
+
+    class Config:
+        title = "Average"
+
+
+class BlurMedian(Config):
+    kernelSize: KernelSize
+    name: Literal["BlurMedian"] = "BlurMedian"
+    value: Literal["BlurMedian"] = "BlurMedian"
+    type: Literal["string"] = "string"
+    field: Literal["option"] = "option"
+
+    class Config:
+        title = "Median"
+
+
+class BlurBilateral(Config):
+    kernelSize: KernelSize
+    name: Literal["BlurBilateral"] = "BlurBilateral"
+    value: Literal["BlurBilateral"] = "BlurBilateral"
+    type: Literal["string"] = "string"
+    field: Literal["option"] = "option"
+
+    class Config:
+        title = "Bilateral"
+
+
+class BlurType(Config):
+    """
+        Blur Type can be selected from here
+    """
+    name: Literal["BlurType"] = "BlurType"
+    value: Union[BlurGaussian, BlurAverage, BlurMedian, BlurBilateral]
+    type: Literal["object"] = "object"
+    field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
+
+    class Config:
+        title = "Blur Type"
+
+
+class ImageFocusedInputs(Inputs):
     inputImage: InputImage
 
 
-class PackageConfigs(Configs):
-    degree: Degree
-    drawBBox: KeepSideBBox
+class DetectionFocusedInputs(Inputs):
+    inputImage: InputImage
+    inputDetections: InputDetections
 
 
-class PackageOutputs(Outputs):
+class ImageFocusedConfigs(Configs):
+    blurType: BlurType
+
+
+class DetectionFocusedConfigs(Configs):
+    blurType: BlurType
+
+
+class ImageFocusedOutputs(Outputs):
     outputImage: OutputImage
 
 
-class PackageRequest(Request):
-    inputs: Optional[PackageInputs]
-    configs: PackageConfigs
+class DetectionFocusedOutputs(Outputs):
+    outputImage: OutputImage
+
+
+class ImageFocusedRequest(Request):
+    inputs: Optional[ImageFocusedInputs]
+    configs: ImageFocusedConfigs
 
     class Config:
         json_schema_extra = {
@@ -108,18 +160,47 @@ class PackageRequest(Request):
         }
 
 
-class PackageResponse(Response):
-    outputs: PackageOutputs
+class DetectionFocusedRequest(Request):
+    inputs: Optional[DetectionFocusedInputs]
+    configs: DetectionFocusedConfigs
+
+    class Config:
+        json_schema_extra = {
+            "target": "configs"
+        }
 
 
-class PackageExecutor(Config):
-    name: Literal["Package"] = "Package"
-    value: Union[PackageRequest, PackageResponse]
+class ImageFocusedResponse(Response):
+    outputs: ImageFocusedOutputs
+
+
+class DetectionFocusedResponse(Response):
+    outputs: DetectionFocusedOutputs
+
+
+class ImageFocusedExecutor(Config):
+    name: Literal["ImageFocused"] = "ImageFocused"
+    value: Union[ImageFocusedRequest, ImageFocusedResponse]
     type: Literal["object"] = "object"
     field: Literal["option"] = "option"
 
     class Config:
-        title = "Package"
+        title = "Image Focused Blur"
+        json_schema_extra = {
+            "target": {
+                "value": 0
+            }
+        }
+
+
+class DetectionFocusedExecutor(Config):
+    name: Literal["DetectionFocused"] = "DetectionFocused"
+    value: Union[DetectionFocusedRequest, DetectionFocusedResponse]
+    type: Literal["object"] = "object"
+    field: Literal["option"] = "option"
+
+    class Config:
+        title = "Detection Focused Blur"
         json_schema_extra = {
             "target": {
                 "value": 0
@@ -129,15 +210,12 @@ class PackageExecutor(Config):
 
 class ConfigExecutor(Config):
     name: Literal["ConfigExecutor"] = "ConfigExecutor"
-    value: Union[PackageExecutor]
+    value: Union[DetectionFocusedExecutor, ImageFocusedExecutor]
     type: Literal["executor"] = "executor"
     field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
 
     class Config:
         title = "Task"
-        json_schema_extra = {
-            "target": "value"
-        }
 
 
 class PackageConfigs(Configs):
@@ -147,4 +225,4 @@ class PackageConfigs(Configs):
 class PackageModel(Package):
     configs: PackageConfigs
     type: Literal["component"] = "component"
-    name: Literal["Package"] = "Package"
+    name: Literal["Blux"] = "Blux"
